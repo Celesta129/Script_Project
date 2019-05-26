fa0b871ea167c2fec1208a68325cf03f7bace0a7
KimKT <celeta129@naver.com>
2019-05-26 19:00:06 +0900

    xml 데이터 받기 완료
    print된 주소 누르면 데이터 나옴

--- weather.py
+++ weather.py
@@ -1,14 +1,29 @@
 from urllib.request import urlopen
 from urllib.parse import urlparse
+
+import http.client
+import urllib.request
+
 import urllib
 # 일반 인증키
-key = 'sqzrjxFFd5WX3hf5KetFKPC9StDxln3sbsk3V2CzIg2yMKCYAQmrTayFhk78aYuXs2ToJNRSNP%2FweK%2FroyJ%2Fng%3D%3D'
+key = 'ServiceKey=' + 'sqzrjxFFd5WX3hf5KetFKPC9StDxln3sbsk3V2CzIg2yMKCYAQmrTayFhk78aYuXs2ToJNRSNP%2FweK%2FroyJ%2Fng%3D%3D'
 # End Point : Request는 이쪽으로
 EndPoint = 'http://newsky2.kma.go.kr/service/SecndSrtpdFrcstInfoService2'
+base_date = 'base_date=' + '20190526'
+base_time = 'base_time=' + '1800'
+nx = 'nx=' + '55'
+ny = 'ny=' + '127'
+
 #메세지명 http://newsky2.kma.go.kr/service/SecndSrtpdFrcstInfoService2/ForecastSpaceData
 url = 'http://newsky2.kma.go.kr/service/SecndSrtpdFrcstInfoService2/ForecastSpaceData?' \
-      'ServiceKey=sqzrjxFFd5WX3hf5KetFKPC9StDxln3sbsk3V2CzIg2yMKCYAQmrTayFhk78aYuXs2ToJNRSNP%2FweK%2FroyJ%2Fng%3D%3D' \
-      'base_date=20190526&base_time=1500&nx=55&ny=127&' \
+      + key + '&'\
+      + base_date + '&'\
+      + base_time + '&'\
+      + nx + '&' \
+      + ny
 
 request = urllib.request.Request(url)
 response = urllib.request.urlopen(request)
+
+print(url)
+
